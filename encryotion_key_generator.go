848a4eaed2da4b6b937d6621ee535bbce6e945e7
Blessing Nyaberi Hamisi <nyaberihamisi@gmail.com>
2025-07-11 18:20:48 +0300

    Initial commit: encryption & decryption

---
+++ encryotion_key_generator.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Your AES-256 Key (Base64):", base64.StdEncoding.EncodeToString(key))
+}
